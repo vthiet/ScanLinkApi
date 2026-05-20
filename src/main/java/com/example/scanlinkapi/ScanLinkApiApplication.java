911bee37b5593b9a9f842f8010ea444260bdcbc9
vthiet <thietvv2005@outlook.com>
2026-05-20 21:28:36 +0700

    create ScanLinkAPI

---
+++ src/main/java/com/example/scanlinkapi/ScanLinkApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.scanlinkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanLinkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScanLinkApiApplication.class, args);
+    }
+
+}
