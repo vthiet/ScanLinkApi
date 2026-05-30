641923f33a4763897e559a0590210452fc51b842
Vi Van Hieu <nobihaza115@gmail.com>
2026-05-30 23:36:12 +0700

    add ocr

---
+++ src/main/java/com/example/scanlink/api/service/CreateFileOutput.java
@@ -0,0 +1,8 @@
+package com.example.scanlink.api.service;
+
+
+import java.io.File;
+
+public interface CreateFileOutput {
+    public File CreateFile(String text) throws Exception;
+}
---
+++ src/main/java/com/example/scanlink/api/service/FilePdf.java
@@ -0,0 +1,27 @@
+package com.example.scanlink.api.service;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+
+import java.io.File;
+
+import java.io.File;
+
+public class FilePdf implements CreateFileOutput {
+    @Override
+    public File CreateFile(String text) throws Exception {
+        String path = "output.pdf";
+
+        PdfWriter writer = new PdfWriter(path);
+        PdfDocument pdf = new PdfDocument(writer);
+        Document document = new Document(pdf);
+
+        document.add(new Paragraph(text));
+
+        document.close();
+
+        return new File(path);
+    }
+}
