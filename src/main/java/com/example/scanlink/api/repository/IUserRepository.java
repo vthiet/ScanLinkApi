6d52272170db0f3efb09f2a90d83f1988c863b0a
vthiet <vthiet@ubuntu.lan>
2026-06-24 16:56:25 +0700

    feat: add Docker configuration and deployment script while migrating repository to MongoDB

--- src/main/java/com/example/scanlink/api/repository/IUserRepository.java
+++ src/main/java/com/example/scanlink/api/repository/IUserRepository.java
@@ -1,7 +1,6 @@
 package com.example.scanlink.api.repository;
 
 import com.example.scanlink.api.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
